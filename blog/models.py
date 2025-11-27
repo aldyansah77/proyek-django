5cd171ff2236ef6e9b6a4468d0b1812055be1a1f
aldyansah77 <aldyansahsmk01@gmail.com>
2025-11-27 21:43:58 +0700

    Menambahkan fitur model dan admin untuk Post

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -9,8 +8,10 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    def __str__(self):
+
+    def _str_(self):
         return self.title
